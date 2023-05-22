130f8b03b62cb2fd809355127c1859254120ea9c
Quaglia98 <78266578+Quaglia98@users.noreply.github.com>
2023-05-22 22:01:25 +0200

    DNS and mining pools analysis
    I added functions to analyze DNS requests for known cryptojacking domains and network traffic for known mining pools.

--- Code/network_analysis/src/main.rs
+++ Code/network_analysis/src/main.rs
@@ -6,10 +6,10 @@ use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::tcp::TcpFlags;
 use pnet::packet::{tcp::TcpPacket, Packet};
+use regex::Regex;
 
 const CRYPTOJACKING_PORTS: &[u16] = &[3333, 4444, 5555, 9009];
 const CRYPTOJACKING_PAYLOAD: &[u8] = &[0x41, 0x41, 0x41];
-
 
 trait IpPacketInfo {
     fn get_source(&self) -> String;
@@ -103,7 +103,8 @@ fn analyze_tcp_packet(ip_packet: &dyn IpPacketInfo, tcp_packet: &TcpPacket) {
     let flags = tcp_packet.get_flags();
 
     // Check for specific ports commonly used in cryptojacking
-    if CRYPTOJACKING_PORTS.contains(&source_port) || CRYPTOJACKING_PORTS.contains(&destination_port) {
+    if CRYPTOJACKING_PORTS.contains(&source_port) || CRYPTOJACKING_PORTS.contains(&destination_port)
+    {
         let source_ip = ip_packet.get_source();
         let destination_ip = ip_packet.get_destination();
 
@@ -127,15 +128,61 @@ fn analyze_tcp_packet(ip_packet: &dyn IpPacketInfo, tcp_packet: &TcpPacket) {
             );
         }
 
-        // Warning message outside if conditions
-        warn!(
-            "Potential cryptojacking detected! Source IP: {}, Destination IP: {}",
-            source_ip, destination_ip
-        );
+
+        //Analyzing DNS requests for cryptojacking domains
+        let dns_payload = tcp_packet.payload(); // Assuming DNS payload is available
+        if is_cryptojacking_dns_request(&dns_payload) {
+            warn!(
+                "Potential cryptojacking DNS request detected! Source IP: {}, Destination IP: {}",
+                source_ip, destination_ip
+            );
+        }
+
+        //Analyzing network traffic for connections to known mining pools
+        if is_connection_to_mining_pool(&source_ip, &destination_ip, source_port, destination_port)
+        {
+            warn!(
+                "Potential connection to cryptojacking mining pool detected! Source IP: {}, Destination IP: {}",
+                source_ip, destination_ip
+            );
+        }
     }
 }
 
 
+fn is_cryptojacking_dns_request(dns_payload: &[u8]) -> bool {
+    // Convert the DNS payload to a string for analysis
+    let dns_request = String::from_utf8_lossy(dns_payload).to_lowercase();
+
+    // Define a regular expression pattern to match cryptojacking-related domains
+    let cryptojacking_domain_pattern = Regex::new(r"(coinhive|minero|cryptoloot|coinimp|coinerra|crypto-loot)").unwrap();
+
+    // Check if the DNS request matches the cryptojacking domain pattern
+    cryptojacking_domain_pattern.is_match(&dns_request)
+}
+
+
+
+fn is_connection_to_mining_pool(
+    _source_ip: &str,
+    destination_ip: &str,
+    _source_port: u16,
+    destination_port: u16,
+) -> bool {
+    // Define known mining pools with their IP addresses and ports
+    let known_mining_pools = [
+        ("eu1.ethermine.org", 4444),
+        ("us1.ethermine.org", 4444),
+        ("eu1.nicehash.com", 3333),
+        ("us-east.stratum.slushpool.com", 3333),
+        // Add more mining pools as needed
+    ];
+
+    // Check if the provided IP addresses and ports match any of the known mining pool addresses and ports
+    known_mining_pools
+        .iter()
+        .any(|(pool_ip, pool_port)| *pool_ip == destination_ip && *pool_port == destination_port)
+}
 
 fn main() {
     // Obtain the network interface for packet capturing
